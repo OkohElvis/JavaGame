948b337a0e1c030a77f4bf6b2328664c796b8a52
Elvis <elvis.okoh@ucdconnect.ie>
2023-09-25 01:26:18 +0100

    simple character creation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,68 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public enum archetype {warrior, mage, gunslinger, summoner}
+    static Scanner reader = new Scanner(System.in);
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Welcome to Battle simulator");
+        var player1 = new Player();
+        characterName(player1);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Welcome "+player1.name+" to the battle showdown!");
+        System.out.println("");
+        //delay
+        try {
+            // Adding a 3-second delay (3000 milliseconds)
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            // Handle the exception
+            e.printStackTrace();
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        playerClass(player1);//get player class
+
+    }
+
+    public static void characterName(Player player1){
+        System.out.println("Enter character name: ");
+        player1.name = reader.nextLine();
+
+
+    }
+
+    public static void playerClass(Player player1){
+        System.out.println("Select a class");
+        System.out.println("1.Warrior");
+        System.out.println("2.Mage");
+        System.out.println("3.Summoner");
+        System.out.println("4.Gunslinger");
+        int playerClass = reader.nextInt();
+        System.out.print("You selected ");
+
+        switch (playerClass){
+            case 1:
+                System.out.print("warrior");
+                player1.warrior = true;
+                player1.physicalAttack+=10;
+                break;
+            case 2:
+                System.out.print("mage");
+                player1.mage = true;
+                player1.magicAttack+=10;
+                break;
+            case 3:
+                System.out.print("summoner");
+                player1.summoner=true;
+                player1.summonDamage+=10;
+            case 4:
+                System.out.print("gunslinger");
+                player1.gunslinger=true;
+                player1.rangedAttack+=10;
+
         }
     }
+
 }
+
---
+++ src/Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    String name = "";
+    boolean mage = false;
+    boolean gunslinger = false;
+    boolean summoner = false;
+    boolean warrior = false;
+    int level = 1;
+    boolean transformation = false;
+    int health = 10;
+    int physicalAttack = 0;
+    int magicAttack = 0;
+    int rangedAttack = 0;
+    int summonDamage = 0;
+
+}
